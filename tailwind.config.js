a0c1d2ff9a7c60c0c4013ed149750428e4a9faaa
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-07-06 18:02:17 -0600

    set theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'water-blue': '#36B3E8',
+        'bubblegum-pink': '#FF89D3',
+        'medium-green': '#6ADB6B',
+        lilac: '#C38EFA',
+        'pale-muave': '#FDC5FA',
+        butterscotch: '#FBBA57',
+        'aqua-blue': '#27D7E7',
+        'dark-orange': '#F76E35',
+        'off-yellow': '#FBF845',
+        'red-pink': '#EC1D51',
+        strawberry: '#FF2745',
+      },
+    },
   },
   plugins: [],
 };
