6e2ca4afbe844863d9a0a51684dda8a08c2ba997
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-12-02 06:05:02 -0700

    implement fetch hook into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-
 import Header from './components/Header';
 import TextShadow from './components/TextShadow';
 import { color } from './consts/color';
 import './index.css';
+import { useFetch } from './hooks/useFetch';
+
+const url = `https://api.discogs.com/lists/1410808 --user-agent "bassbook/0.1"`;
+const fetchOptions: RequestInit = {
+  mode: 'cors',
+};
 
 function App() {
+  const { data, error } = useFetch(url, fetchOptions);
 
+  if (error) return <p>There is an error: {error.message}</p>;
+  if (!data) return <p>Loading...</p>;
   return (
     <>
       <Header />
 
-      <div title='BASSBOOK ' className="text-6xl md:text-7xl lg:text-7xl xl:text-8xl text-center mt-11 font-orbitron text-cream">
+      <div
+        title="BASSBOOK "
+        className="text-6xl md:text-7xl lg:text-7xl xl:text-8xl text-center mt-11 font-orbitron text-cream"
+      >
         <TextShadow letter="B" shadowColor={color.bubblegumPink} />
         <TextShadow letter="A" shadowColor={color.lightGreen} />
         <TextShadow letter="S" shadowColor={color.aquaBlue} />
