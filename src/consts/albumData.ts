5b28edf6c0d6d3d9ea203efbb6a01ead50c386e7
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-12-04 11:47:29 -0700

    add album art and youtube urls

---
+++ src/consts/albumData.ts
@@ -0,0 +1,35 @@
+export const imageUrls = {
+  'Moe Shop - Moe Moe':
+    'https://i.discogs.com/IHP_5jVM5aIYUWZf2IelBPela-2Tx2USZWx2ccQwHZU/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNzE1/NTM1LTE1NDA1OTIz/MTgtMjM5MS5qcGVn.jpeg',
+  'Mameyudoufu - Showcase':
+    'https://i.discogs.com/NZ69qb2ouGrnrBeC9SjTJ5qkM6WCyPgMm7m2LQrWJMw/rs:fit/g:sm/q:90/h:592/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzMTc5/MDEzLTE1NDk0Mzg3/NzktMzQ0OS5wbmc.jpeg',
+  'Such (3) - Innocent':
+    'https://i.discogs.com/gAUFEes2Dudeuo0CSdhkGiECoAwZtF9vIO_Zt6JEsns/rs:fit/g:sm/q:90/h:592/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzNjQy/NjEyLTE1NTgxMjI3/NjctMzkzMC5wbmc.jpeg',
+  "Yuc'e - Sugary Galaxy":
+    'https://i.discogs.com/x5ozNIvOWkBi-g8IIq3oujFwHgr5geJREBRCKxrEBJE/rs:fit/g:sm/q:90/h:544/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI3NzI0/NDU4LTE2ODk3OTY1/NDktNjgyNy5qcGVn.jpeg',
+  'Yunomi (2) - ゆのもきゅ':
+    'https://i.discogs.com/kcZ29z7Awm2OZY76j2wi7czqV7w7N93FcG-6a3du1lQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExMDg4/OTkzLTE1MDk2NTA1/MTUtNzc5MS5wbmc.jpeg',
+  'Dark Cat - Finite ':
+    'https://i.discogs.com/pYCUsn9eQhb6BUjziZCanGOVtu6jSB_5lqAHnqvtk4U/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg2MTE0/OTctMTQ2NTExMzkw/Mi03ODE1LmpwZWc.jpeg',
+  'Tanuki - 漢字・タイトル / カタカナ・タイトル = Kanji Title / Katakana Title':
+    'https://i.discogs.com/rU27_bSbyKbDhtwkDtCcN4ge-0H-xfG6TZStO4RhiiQ/rs:fit/g:sm/q:90/h:609/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzMTQy/OTMwLTE1NDg4MDY0/MzEtMzA4NC5wbmc.jpeg',
+  'Happy Kuru Kuru - Happy Kuru Kuru':
+    'https://i.discogs.com/JFQX5nogaz0-j8xBWwbofpNdyjJnh-C9bHZGywqWd90/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExNjU3/NzgwLTE1MjAxNjQx/NTYtNzM0Ny5qcGVn.jpeg',
+  'Psyqui - Your Voice So...':
+    'https://i.discogs.com/MsPLyS17YAaHZlcbXZemwfp34Gv6tAxb21hBdOxg4f8/rs:fit/g:sm/q:90/h:470/w:470/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyNzgy/ODQyLTE1NDE4NTkw/MzktOTYzNy5qcGVn.jpeg',
+};
+
+export const youtubeUrls = {
+  'Moe Shop - Moe Moe': 'https://youtu.be/F9Ay74LfKd4?si=7VxAa3Ii9rBsM2BJ',
+  'Mameyudoufu - Showcase': 'https://youtu.be/-Ffv8L8QDA4?si=NVxSGlTV7lYrv2IJ',
+  'Such (3) - Innocent': 'https://youtu.be/lCY92xb5CuY?si=YdyMxsrykqlpeYP4',
+  "Yuc'e - Sugary Galaxy": 'https://youtu.be/rBRaYxaBNVU?si=v_1IM56TfP0fXvCK',
+  'Yunomi (2) - ゆのもきゅ': 'https://youtu.be/I7-EWVVWFa8?si=Q1DCyvkHSjpA6Eqj',
+  'Dark Cat - Finite ': 'https://youtu.be/y69QZxO4G2s?si=wvg8qjb_IafMt897',
+  'Tanuki - 漢字・タイトル / カタカナ・タイトル = Kanji Title / Katakana Title':
+    'https://youtu.be/uthfuVYdJV4?si=HZE8rpV-vDxHWtYx',
+  'Happy Kuru Kuru - Happy Kuru Kuru':
+    'https://www.youtube.com/watch?v=cR5gOQ3aREo',
+  'Psyqui - Your Voice So...':
+    'https://youtu.be/3Qsw0lQZduA?si=1DCaqzXbfS_5jLqD',
+};
