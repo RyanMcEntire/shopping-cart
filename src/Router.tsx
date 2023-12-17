8ea5ee4b99897d9b3e3ed84123605f83445885af
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-12-17 12:08:52 -0700

    remove unused imports

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Shop from './components/Shop';
