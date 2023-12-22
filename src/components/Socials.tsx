760ac4b97b3e8444b43dc4a973436706232c57af
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-12-21 22:08:14 -0700

    add extension change for 8xl font size

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,4 +1,4 @@
-import { AwesomeButton, AwesomeButtonSocial } from 'react-awesome-button';
+import { AwesomeButtonSocial } from 'react-awesome-button';
 import Header from './Header';
 
 const Socials = () => {
@@ -6,9 +6,11 @@ const Socials = () => {
     <>
       <Header />
       <div className="flex flex-col max-w-md gap-4 m-auto mt-20 text-cream">
-        <div className='text-2xl text-center font-semi-bold font-asap'>Thanks for checking this out</div>
+        <div className="text-2xl text-center font-semi-bold font-asap">
+          Thanks for checking this out
+        </div>
         <AwesomeButtonSocial
-        className='m-4'
+          className="m-4"
           type="github"
           href="https://github.com/RyanMcEntire/shopping-cart"
         >
